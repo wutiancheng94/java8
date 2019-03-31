608f7b925dfe2ff0692c3ee9f04d4971ffd64a2c
wutiancheng <975930271@qq.com>
2019-03-31 14:24:30 +0800

    将List类型抽象化，使filter方法过滤能力更强

---
+++ src/main/java/com/wtc/study/java8/lambda/demo06/FilterApple06.java
@@ -0,0 +1,39 @@
+package com.wtc.study.java8.lambda.demo06;
+
+import com.wtc.study.java8.lambda.demo01.Apple;
+import com.wtc.study.java8.lambda.demo02.Predicate;
+import com.wtc.study.java8.lambda.utils.AppleUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 将List类型抽象化，使filter方法过滤能力更强
+ *
+ * @author 吴天成
+ * @create 2019/3/31
+ * @since 1.0.0
+ */
+public class FilterApple06 {
+
+    public static void main(String[] args) {
+        List<Apple> apples = AppleUtils.getApples();
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+
+        System.out.println(filter(apples, (p) -> "red".equals(p.getColor())));
+
+        System.out.println(filter(numbers, (i) -> i % 2 == 0));
+    }
+
+    private static <T> List<T> filter(List<T> list, Predicate<T> predicate) {
+        List<T> newList = new ArrayList<>();
+        for (T t : list) {
+            if (predicate.test(t)) {
+                newList.add(t);
+            }
+        }
+
+        return newList;
+    }
+}
